3afe0330555b0c8d3159af21caa703b92b46c9ea
ketchanji <valerydjanya8@gmail.com>
2022-12-21 05:38:39 +0800

    first commit

--- src/component/UI/index.jsx
+++ src/component/UI/index.jsx
@@ -1,6 +1,6 @@
-import Button from "./Button";
-import Form from "./form";
-import Hero from "./Hero";
-import NavBar from "../NavBar";
+import Button from "../UI/Button";
+import Form from "../UI/form";
+import Hero from "../UI/Hero";
+import NavBar from "../UI/NavBar";
 
 export { Button, Form, Hero, NavBar };
